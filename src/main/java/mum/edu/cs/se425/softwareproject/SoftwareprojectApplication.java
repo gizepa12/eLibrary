58f46077cc8ceb36683c9859b6bd79214851ea5e
gizepa12 <gizawroba@gmail.com>
2019-10-09 12:33:12 -0500

    new project

---
+++ src/main/java/mum/edu/cs/se425/softwareproject/SoftwareprojectApplication.java
@@ -0,0 +1,14 @@
+package mum.edu.cs.se425.softwareproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareprojectApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SoftwareprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mum/edu/cs/se425/softwareproject/SoftwareprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package mum.edu.cs.se425.softwareproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoftwareprojectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
